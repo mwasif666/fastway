571f0520d6ef3bfd273ae203c5f47c68b6794f8c
shaheryarali938 <shaheryarali910@gmail.com>
2024-11-27 14:27:51 +0500

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import Home1Page from "./app/components/home/home1";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Home1Page />);
