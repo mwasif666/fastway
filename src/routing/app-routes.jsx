8bb4ac1c528961337c63505083d317bc614bac5c
shaheryarali938 <shaheryarali910@gmail.com>
2024-11-27 12:14:52 +0500

    done

--- src/routing/app-routes.jsx
+++ src/routing/app-routes.jsx
@@ -34,38 +34,110 @@ import ShopChekoutPage from "../app/components/shop/checkout";
 import ElementsPage from "../app/components/elements";
 
 function AppRoutes() {
-    return (
-        <Routes>
-            <Route path={route.INITIAL} element={<Home1Page />}/>
-            <Route path={route.home.HOME1} element={<Home1Page />}/>
-            <Route path={route.home.HOME2} element={<Home2Page />}/>
-            <Route path={route.home.HOME3} element={<Home3Page />}/>
-            <Route path={route.pages.ABOUT} element={<InnerPageLayout content={<AboutUsPage />} />}/>
-            <Route path={route.pages.FAQ} element={<InnerPageLayout content={<FAQPage />} />}/>
-            <Route path={route.pages.team.TEAM} element={<InnerPageLayout content={<TeamPage />} />}/>
-            <Route path={route.pages.team.DETAIL} element={<InnerPageLayout content={<TeamDetailPage />} />}/>
-            <Route path={route.pages.contact.CONTACT1} element={<InnerPageLayout content={<ContactUs1Page />} />}/>
-            <Route path={route.pages.contact.CONTACT2} element={<InnerPageLayout content={<ContactUs2Page />} />}/>
-            <Route path={route.pages.gallery.GALLERY1} element={<InnerPageLayout content={<Gallery1Page />} />}/>
-            <Route path={route.pages.gallery.GALLERY2} element={<InnerPageLayout content={<Gallery2Page />} />}/>
-            <Route path={route.pages.features.footer.FIXED} element={<InnerPageLayout content={<FeaturesPage />} />}/>
-            <Route path={route.pages.features.footer.LIGHT} element={<InnerPageLayout content={<FeaturesPage />} />}/>
-            <Route path={route.pages.features.footer.DARK} element={<InnerPageLayout content={<FeaturesPage />} />}/>
-            <Route path={route.services.SERVICES} element={<InnerPageLayout content={<ServicesPage />} />}/>
-            <Route path={route.services.DETAIL} element={<InnerPageLayout content={<ServiceDetailPage />} />}/>
-            <Route path={route.blog.LIST} element={<InnerPageLayout content={<BlogListPage />} />}/>
-            <Route path={route.blog.LIST_SIDEBAR} element={<InnerPageLayout content={<BlogListSidebarPage />} />}/>
-            <Route path={route.blog.GRID} element={<InnerPageLayout content={<BlogGridPage />} />}/>
-            <Route path={route.blog.GRID_SIDEBAR} element={<InnerPageLayout content={<BlogGridSidebarPage />} />}/>
-            <Route path={route.blog.POST_SINGLE} element={<InnerPageLayout content={<BlogPostSinglePage />} />}/>
-            <Route path={route.shop.PRODUCTS} element={<InnerPageLayout content={<ShopProductsPage />} />}/>
-            <Route path={route.shop.DETAIL} element={<InnerPageLayout content={<ShopProductDetailPage />} />}/>
-            <Route path={route.shop.CART} element={<InnerPageLayout content={<ShopCartPage />} />}/>
-            <Route path={route.shop.WISHLIST} element={<InnerPageLayout content={<ShopWishlistPage />} />}/>
-            <Route path={route.shop.CHECKOUT} element={<InnerPageLayout content={<ShopChekoutPage />} />}/>
-            <Route path={route.ELEMENTS} element={<InnerPageLayout content={<ElementsPage />} />}/>
-            <Route path="*" element={<InnerPageLayout content={<Error404 />} />}/>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Home1Page />} />
+      <Route path={route.home.HOME1} element={<Home1Page />} />
+      <Route path={route.home.HOME2} element={<Home2Page />} />
+      <Route path={route.home.HOME3} element={<Home3Page />} />
+      <Route
+        path={route.pages.ABOUT}
+        element={<InnerPageLayout content={<AboutUsPage />} />}
+      />
+      <Route
+        path={route.pages.FAQ}
+        element={<InnerPageLayout content={<FAQPage />} />}
+      />
+      <Route
+        path={route.pages.team.TEAM}
+        element={<InnerPageLayout content={<TeamPage />} />}
+      />
+      <Route
+        path={route.pages.team.DETAIL}
+        element={<InnerPageLayout content={<TeamDetailPage />} />}
+      />
+      <Route
+        path={route.pages.contact.CONTACT1}
+        element={<InnerPageLayout content={<ContactUs1Page />} />}
+      />
+      <Route
+        path={route.pages.contact.CONTACT2}
+        element={<InnerPageLayout content={<ContactUs2Page />} />}
+      />
+      <Route
+        path={route.pages.gallery.GALLERY1}
+        element={<InnerPageLayout content={<Gallery1Page />} />}
+      />
+      <Route
+        path={route.pages.gallery.GALLERY2}
+        element={<InnerPageLayout content={<Gallery2Page />} />}
+      />
+      <Route
+        path={route.pages.features.footer.FIXED}
+        element={<InnerPageLayout content={<FeaturesPage />} />}
+      />
+      <Route
+        path={route.pages.features.footer.LIGHT}
+        element={<InnerPageLayout content={<FeaturesPage />} />}
+      />
+      <Route
+        path={route.pages.features.footer.DARK}
+        element={<InnerPageLayout content={<FeaturesPage />} />}
+      />
+      <Route
+        path={route.services.SERVICES}
+        element={<InnerPageLayout content={<ServicesPage />} />}
+      />
+      <Route
+        path={route.services.DETAIL}
+        element={<InnerPageLayout content={<ServiceDetailPage />} />}
+      />
+      <Route
+        path={route.blog.LIST}
+        element={<InnerPageLayout content={<BlogListPage />} />}
+      />
+      <Route
+        path={route.blog.LIST_SIDEBAR}
+        element={<InnerPageLayout content={<BlogListSidebarPage />} />}
+      />
+      <Route
+        path={route.blog.GRID}
+        element={<InnerPageLayout content={<BlogGridPage />} />}
+      />
+      <Route
+        path={route.blog.GRID_SIDEBAR}
+        element={<InnerPageLayout content={<BlogGridSidebarPage />} />}
+      />
+      <Route
+        path={route.blog.POST_SINGLE}
+        element={<InnerPageLayout content={<BlogPostSinglePage />} />}
+      />
+      <Route
+        path={route.shop.PRODUCTS}
+        element={<InnerPageLayout content={<ShopProductsPage />} />}
+      />
+      <Route
+        path={route.shop.DETAIL}
+        element={<InnerPageLayout content={<ShopProductDetailPage />} />}
+      />
+      <Route
+        path={route.shop.CART}
+        element={<InnerPageLayout content={<ShopCartPage />} />}
+      />
+      <Route
+        path={route.shop.WISHLIST}
+        element={<InnerPageLayout content={<ShopWishlistPage />} />}
+      />
+      <Route
+        path={route.shop.CHECKOUT}
+        element={<InnerPageLayout content={<ShopChekoutPage />} />}
+      />
+      <Route
+        path={route.ELEMENTS}
+        element={<InnerPageLayout content={<ElementsPage />} />}
+      />
+      <Route path="*" element={<InnerPageLayout content={<Error404 />} />} />
+    </Routes>
+  );
 }
 export default AppRoutes;
